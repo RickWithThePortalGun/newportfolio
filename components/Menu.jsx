92b7e54e9c52731ef7f60cdeee07214f88da5123
Oyeniyi Victor <flamezy36@gmail.com>
2024-02-01 08:54:36 +0100

    last attempt

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -103,15 +103,13 @@ const wrapperVariants = {
   open: {
     scaleY: 1,
     transition: {
-      when: "beforeChildren",
-      staggerChildren: 0.1,
+      when:"beforeChildren",
     },
   },
   closed: {
     scaleY: 0,
     transition: {
       when: "afterChildren",
-      staggerChildren: 0.1,
     },
   },
 };
